226327eb564751baf4720f7443643379de81b6a3
Aleksey <fduchunru@gmail.com>
2020-10-14 03:44:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ function breakOut(array, changeValue, stopValue){
     } return array
   }
 
-function keepGoing(array, changeValue, skipValue){
-
-}
+  function keepGoing(array, changeValue, skipValue){
+       for (let i = 0; i < array.length; i++) {
+           if (array[i] !== skipValue) {
+               array[i] = changeValue
+           }else{
+               continue
+           }
+       } return array
+   }
